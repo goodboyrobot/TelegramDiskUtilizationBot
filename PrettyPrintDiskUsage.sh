6ca509051a1b6d625d712f2926db97d6160438d7
Alex Schuldberg <afschuld@gmail.com>
2024-04-03 15:35:07 -0700

    feature complete

--- PrettyPrintDiskUsage.sh
+++ PrettyPrintDiskUsage.sh
@@ -6,6 +6,12 @@ for filename in /media/nas/*; do
 		then du -sh $filename 2>/dev/null
 	fi
 done
+echo "--Main Library--"
+du -sh "/media/nas/afschuld/TV Shows" 2>/dev/null
+du -sh "/media/nas/afschuld/Movies" 2>/dev/null
+du -sh "/media/nas/afschuld/4kMovies" 2>/dev/null
+du -sh "/media/nas/afschuld/Anime_TV_Shows" 2>/dev/null
+du -sh "/media/nas/afschuld/Anime_Movies" 2>/dev/null
 echo "--Total--"
 du -sh "/media/nas/" 2>/dev/null
 
--- robot.py
+++ robot.py
@@ -6,7 +6,7 @@ import threading
 import subprocess
 import re
 # import credentials from creds.py
-from creds import BOT_TOKEN, CHATBOT_HANDLE, CHAT_ID
+from creds import BOT_TOKEN, CHATBOT_HANDLE, CHAT_ID, TAUTULLI_API_KEY, TAUTULLI_LOCATION
 
 # Function that sends a message to a specific telegram group
 def telegram_bot_sendtext(bot_message,chat_id):
@@ -20,6 +20,7 @@ def telegram_bot_sendtext(bot_message,chat_id):
         json=data,
         timeout=5
     )
+    print(response.json())
     return response.json()
 
 # 3b. Function that sends an image to a specific telegram group
@@ -33,15 +34,34 @@ def telegram_bot_sendimage(image_url, group_id):
     response = requests.post(url, data=data, timeout=5)
     return response.json()
 
+def get_tautulli_stats():
+    response = requests.post("http://" + TAUTULLI_LOCATION + "/api/v2?apikey=" + TAUTULLI_API_KEY + "&cmd=get_home_stats&time_range=7").json()
+    #print(response['response']['data'])
+    stats= "Most Popular Movie of last week: " + response['response']['data'][1]['rows'][0]['title'] + " (" + str(response['response']['data'][1]['rows'][0]['users_watched']) + " Users)"
+    stats+= "\nMost Popular TV Show of last week: " + response['response']['data'][3]['rows'][0]['title'] + " (" + str(response['response']['data'][3]['rows'][0]['users_watched']) + " Users)"
+    stats+= "\nMost Popular Artist of last week: " + response['response']['data'][9]['rows'][0]['title'] + " (" + str(response['response']['data'][9]['rows'][0]['total_plays']) + " Plays)"
+
+    #print(stats)
+    return stats
+
 # Call 
 def main():
     #output = subprocess.check_output('./PrettyPrintDiskUsage.sh',stderr=subprocess.STDOUT,shell=True)
     #telegram_bot_sendtext('```\n' + output.decode('utf-8') + '```',CHAT_ID)
-    zpool_output = subprocess.check_output('zpool list',stderr=subprocess.STDOUT,shell=True).decode('utf-8')
-    print(zpool_output)
-    free_space_search = re.search('T  (([0-9]+)(\.[0-9])*)T',zpool_output)
-    free_space = free_space_search.group(1)
-    print(free_space)
-# Run the main function
+    zpool_output = subprocess.check_output('zfs list',stderr=subprocess.STDOUT,shell=True).decode('utf-8')
+    #print(zpool_output)
+    free_space_search = re.search('castor\/media.*T  (([0-9]+)(\.[0-9])*)T',zpool_output)
+    free_space = float(free_space_search.group(1))
+    last_week_file = open('lastweek', 'r')
+    lines = last_week_file.readlines()
+    last_week=float(lines[0].strip())
+    #print("Last week's space remaining " + str(last_week) + "TB")
+    burn_rate = (last_week - free_space)/7
+    tautulli_stats = get_tautulli_stats()
+    #have to escape . in the below re.escape() is the easiest way
+    telegram_bot_sendtext(re.escape("Free space remaining: " + str(free_space) + "TB\n"+"Current burn rate: {:.3f}TB per day, {:.1f} days of storage remaining\n{}".format(burn_rate,free_space/burn_rate,tautulli_stats)),CHAT_ID)
+       
+
+    # Run the main function
 if __name__ == "__main__":
     main()
