eeab0421f43432d0d2a5b728b92104ce509fa5af
Alex Schuldberg <afschuld@gmail.com>
2024-04-03 15:47:32 -0700

    fit and finish

--- robot.py
+++ robot.py
@@ -20,7 +20,7 @@ def telegram_bot_sendtext(bot_message,chat_id):
         json=data,
         timeout=5
     )
-    print(response.json())
+    #print(response.json())
     return response.json()
 
 # 3b. Function that sends an image to a specific telegram group
@@ -46,21 +46,24 @@ def get_tautulli_stats():
 
 # Call 
 def main():
-    #output = subprocess.check_output('./PrettyPrintDiskUsage.sh',stderr=subprocess.STDOUT,shell=True)
-    #telegram_bot_sendtext('```\n' + output.decode('utf-8') + '```',CHAT_ID)
+    output = subprocess.check_output('./PrettyPrintDiskUsage.sh',stderr=subprocess.STDOUT,shell=True)
+    telegram_bot_sendtext('```\n' + output.decode('utf-8') + '```',CHAT_ID)
     zpool_output = subprocess.check_output('zfs list',stderr=subprocess.STDOUT,shell=True).decode('utf-8')
     #print(zpool_output)
     free_space_search = re.search('castor\/media.*T  (([0-9]+)(\.[0-9])*)T',zpool_output)
     free_space = float(free_space_search.group(1))
     last_week_file = open('lastweek', 'r')
     lines = last_week_file.readlines()
+    last_week_file.close()
     last_week=float(lines[0].strip())
     #print("Last week's space remaining " + str(last_week) + "TB")
     burn_rate = (last_week - free_space)/7
     tautulli_stats = get_tautulli_stats()
     #have to escape . in the below re.escape() is the easiest way
     telegram_bot_sendtext(re.escape("Free space remaining: " + str(free_space) + "TB\n"+"Current burn rate: {:.3f}TB per day, {:.1f} days of storage remaining\n{}".format(burn_rate,free_space/burn_rate,tautulli_stats)),CHAT_ID)
-       
+    last_week_file = open('lastweek','w')
+    last_week_file.writelines([str(free_space)])
+    last_week_file.close()
 
     # Run the main function
 if __name__ == "__main__":
